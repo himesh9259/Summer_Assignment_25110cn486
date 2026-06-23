c4f0ea7a1621179d6d1e42dbebae8ec5a291eb45
himesh9259 <himeshchaudhary1108@gmail.com>
2026-06-23 19:18:49 +0530

    Add files via upload

---
+++ Day-23/Day-23(Q3).py
@@ -0,0 +1,11 @@
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+# Remove spaces and convert to lowercase
+str1 = str1.replace(" ", "").lower()
+str2 = str2.replace(" ", "").lower()
+
+if sorted(str1) == sorted(str2):
+    print("Strings are Anagrams")
+else:
+    print("Strings are Not Anagrams")
